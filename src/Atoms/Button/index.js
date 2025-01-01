d7a76f1e122e884247230974496bb9d68d3f6a0d
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2025-01-02 02:23:06 +0500

    chore: Add iconLeft and iconRight props to Button component

--- src/Atoms/Button/index.js
+++ src/Atoms/Button/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
 import styles from './Button.module.css';
-const Button = ({ label, onClick, variant = 'primary', disabled = false, type = 'button', customStyle = {} }) => {
+
+const Button = ({
+  label,
+  onClick,
+  variant = 'primary',
+  disabled = false,
+  type = 'button',
+  customStyle = {},
+  iconLeft,
+  iconRight
+}) => {
   const buttonClass = `${styles.button} ${styles[variant]}`;
 
   return (
-    <button 
-      className={buttonClass} 
-      onClick={onClick} 
-      disabled={disabled} 
-      type={type} 
+    <button
+      className={buttonClass}
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
       style={customStyle}
     >
+      {iconLeft && <span className={styles.iconLeft}>{iconLeft}</span>}
+      
       {label}
+
+      {iconRight && <span className={styles.iconRight}>{iconRight}</span>}
     </button>
   );
 };
