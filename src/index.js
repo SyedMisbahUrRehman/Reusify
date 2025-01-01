4909407ef2c8b11dff92623e7a20db891b4f6758
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2025-01-02 02:02:11 +0500

    chore: Add typography styles for different font sizes and weights

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './assets/styles/global.css';
 import './assets/styles/colors.css';
+import './assets/styles/typography.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
